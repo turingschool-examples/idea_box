7eedbeb0e532c4d8d7b08842f49fb4b072e5b371
novohispano <novohispano@gmail.com>
2014-07-28 02:13:14 -0600

    First user feature test

--- test/features/feature_test_helper.rb
+++ test/features/feature_test_helper.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'capybara'
 
 require_relative "../test_helper"
-require_relative '../lib/app.rb'
+require_relative '../../lib/app.rb'
 
 Capybara.app = IdeaBoxApp
 
@@ -12,5 +12,11 @@ class FeatureTest < Minitest::Test
   def teardown
     Capybara.reset_sessions!
     Capybara.use_default_driver
+  end
+end
+
+class Capybara::Session
+  def has_submit_button?(value)
+    self.has_selector?("input[type=submit][value='#{value}']")
   end
 end
--- test/features/user_creates_an_idea.rb
+++ test/features/user_creates_an_idea.rb
@@ -1,7 +1,14 @@
 require_relative "./feature_test_helper"
 
 class UserCreatesAnIdea < FeatureTest
-  def test_user_can_create_article
+  def test_user_can_visit_index
     visit '/'
+    assert_equal 200, page.status_code
+    assert page.has_content?('Feeling Inspired?')
+    assert page.has_css?('h1#intro-header')
+    assert page.has_css?('h4#intro-subheader')
+    assert page.has_link?('IdeaBox')
+    assert page.has_link?('Dashboard')
+    assert page.has_submit_button?('Submit')
   end
 end
