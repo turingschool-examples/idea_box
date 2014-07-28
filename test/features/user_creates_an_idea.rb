1001b34e023b9ce06a879ca1ac9fa2d1547c15e6
novohispano <novohispano@gmail.com>
2014-07-28 04:06:10 -0600

    Delete second user test

--- test/features/user_creates_an_idea.rb
+++ test/features/user_creates_an_idea.rb
@@ -1,11 +1,6 @@
 require_relative "./feature_test_helper"
 
 class UserCreatesAnIdea < FeatureTest
-  def teardown
-    File.delete(File.expand_path("~/Dropbox/projects/tutorials/idea_box/db/ideabox"))
-    super
-  end
-
   def test_user_can_visit_index
     visit '/'
     assert_equal 200, page.status_code
@@ -15,19 +10,5 @@ class UserCreatesAnIdea < FeatureTest
     assert page.has_link?('IdeaBox')
     assert page.has_link?('Dashboard')
     assert page.has_submit_button?('Submit')
-  end
-
-  def test_user_can_create_idea
-    # Capybara.current_driver = :selenium
-
-    visit '/'
-    assert page.has_field?('Title')
-    assert page.has_field?('Description')
-    fill_in('Title',       with: 'One great title')
-    fill_in('Description', with: 'One great description')
-    click_button('Submit')
-
-    assert page.has_content?('One great title')
-    assert page.has_content?('One great description')
   end
 end
