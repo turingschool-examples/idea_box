7b148f32f52a526a0a85f62d4895e02fc655d5ec
novohispano <novohispano@gmail.com>
2014-07-28 04:09:22 -0600

    Initial setup

--- test/features/user_creates_an_idea.rb
+++
@@ -1,4 +0,0 @@
-require_relative "./feature_test_helper"
-
-class UserCreatesAnIdea < FeatureTest
-end
--- test/models/model_test_helper.rb
+++
@@ -1,3 +0,0 @@
-require_relative "../test_helper"
-require_relative "../../lib/app/models/idea"
-require_relative "../../lib/app/models/idea_store"
