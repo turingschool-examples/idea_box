aec846a484f59ed2c9f7910d2e8339ca50f0b5b7
novohispano <novohispano@gmail.com>
2014-07-28 04:07:46 -0600

    Delete first user test

--- test/features/user_creates_an_idea.rb
+++ test/features/user_creates_an_idea.rb
@@ -1,14 +1,4 @@
 require_relative "./feature_test_helper"
 
 class UserCreatesAnIdea < FeatureTest
-  def test_user_can_visit_index
-    visit '/'
-    assert_equal 200, page.status_code
-    assert page.has_content?('Feeling Inspired?')
-    assert page.has_css?('h1#intro-header')
-    assert page.has_css?('h4#intro-subheader')
-    assert page.has_link?('IdeaBox')
-    assert page.has_link?('Dashboard')
-    assert page.has_submit_button?('Submit')
-  end
 end
